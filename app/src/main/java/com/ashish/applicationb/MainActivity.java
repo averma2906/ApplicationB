4325b3fbc94d503411fa2899d03153a831f228ff
Ashish Verma <a.verma.2906@gmail.com>
2019-11-19 20:34:08 +0530

    I have added the operation to the ApplicationB

--- app/src/main/java/com/ashish/applicationb/MainActivity.java
+++ app/src/main/java/com/ashish/applicationb/MainActivity.java
@@ -3,9 +3,17 @@ package com.ashish.applicationb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.content.*;
+enum OperationType{
+    Concatination,
+    Addition,
+    Substraction,
+    Multiplication,
+    Division
+}
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,11 +31,35 @@ public class MainActivity extends AppCompatActivity {
         if(receivedAction.equals(Intent.ACTION_SEND)){
             //content is being shared
             String[] message = intent.getStringArrayExtra("values");
-           Log.d("text",message[0]);
-            Log.d("text",message[1]);
-            TextView textView = (TextView)findViewById(R.id.responseTxt);
 
-            textView.setText(message[0]+message[1]);
+            String currentOperationType =  (message[2]);
+            Log.d("text",message[0]);
+            Log.d("text",message[1]);
+            String result = "";
+            switch (currentOperationType)
+            {
+                case "Concatination" :
+                    result = message[0]+message[1];
+                    break;
+                case "Addition":
+                    int value1 = Integer.parseInt(message[0]);
+                    int value2 = Integer.parseInt(message[1]);
+                     result = "Sum "+(value1+value2);
+                    break;
+                case "Substraction":
+
+                    result = "Substraction "+(Integer.parseInt(message[0])-Integer.parseInt(message[1]));
+                    break;
+
+                case "Multiplication":
+                    result = "Multiplication "+(Integer.parseInt(message[0])*Integer.parseInt(message[1]));
+                    break;
+                case "Division":
+                    result = "Division "+(Integer.parseInt(message[0])/Integer.parseInt(message[1]));
+                    break;
+            }
+            TextView textView = (TextView)findViewById(R.id.responseTxt);
+            textView.setText(result);
         }
         else if(receivedAction.equals(Intent.ACTION_MAIN)){
             //app has been launched directly, not from share list
